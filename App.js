7dc93eaff57c7a3043b340431487d932a9d51f26
Didac Esteban <>
2018-10-22 11:20:39 +0200

    improvements

--- App.js
+++ App.js
@@ -8,14 +8,15 @@ export default class App extends React.Component {
     this.handleCorrectResult = this.handleCorrectResult.bind(this);
     this.handleErrorResult = this.handleErrorResult.bind(this);
     this.handleAnswerButtons = this.handleAnswerButtons.bind(this);
+    this.generateFakeAnswer = this.generateFakeAnswer.bind(this);
+    this.restart = this.restart.bind(this);
     this.state = {
       numberLeft: Math.floor(Math.random() * (10 - 0 + 1)) + 0,
       numberRight: Math.floor(Math.random() * (10 - 0 + 1)) + 0,
+      numberToSelectCorrectAnswerButton: Math.floor(Math.random() * (2 - 0 + 1)) + 0,
       operatorsArray: ['+', '-', '*'],
       operator: '',
       correctAnswer: '',
-      fakeAnswer1: '',
-      fakeAnswer2: '',
       error: null
     };
   }
@@ -46,10 +47,23 @@ export default class App extends React.Component {
     this.setState(prevState => {
       return {
         correctAnswer: eval(`${prevState.numberLeft} ${prevState.operator} ${prevState.numberRight}`),
-        fakeAnswer1: prevState.numberLeft - Math.floor(Math.random() * (10 - 0 + 1)) + 0,
-        fakeAnswer2: prevState.numberRight + Math.floor(Math.random() * (10 - 0 + 1)) + 0,
+        numberToSelectCorrectAnswerButton: Math.floor(Math.random() * (2 - 0 + 1)) + 0
       }
     });
+  }
+
+  generateFakeAnswer() {
+    const randNumber = Math.floor(Math.random() * (10 - 0 + 1)) + 0;
+    const randOperator = this.state.operatorsArray[Math.floor(Math.random() * (2 - 0 + 1)) + 0];
+    const numberToSelectNumberToOperate = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
+
+    let fakeAnswer = eval(`${numberToSelectNumberToOperate === 1 ? this.state.numberLeft : this.state.numberRight} ${randOperator} ${randNumber}`);
+
+    if (fakeAnswer != this.state.correctAnswer) {
+      return fakeAnswer.toString();
+    } else {
+      return eval(`${fakeAnswer} ${this.state.operatorsArray[Math.floor(Math.random() * (1 - 0 + 1)) + 0]} 1`).toString();
+    }
   }
 
   handleCorrectResult() {
@@ -60,28 +74,41 @@ export default class App extends React.Component {
     this.setState({error: 'You lose'});
   }
 
+  restart() {
+    this.setState({error: null});
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>{this.state.numberLeft}</Text>
-        <Text>{this.state.operator}</Text>
-        <Text>{this.state.numberRight}</Text>
-        <Button
-          onPress={this.handleCorrectResult}
-          title={this.state.correctAnswer.toString()}
-          color="#841584"
-        />
-        <Button
-          onPress={this.handleErrorResult}
-          title={this.state.fakeAnswer1.toString()}
-          color="#841584"
-        />
-        <Button
-          onPress={this.handleErrorResult}
-          title={this.state.fakeAnswer2.toString()}
-          color="#841584"
-        />
-        <Text>{this.state.error ? this.state.error : null}</Text>
+      {this.state.error === null ? (
+        <View>
+          <Text style={styles.operation}>{this.state.numberLeft} {this.state.operator} {this.state.numberRight}</Text>
+          <Button
+            onPress={this.state.numberToSelectCorrectAnswerButton == 0 ? this.handleCorrectResult : this.handleErrorResult}
+            title={this.state.numberToSelectCorrectAnswerButton == 0 ? this.state.correctAnswer.toString() : this.generateFakeAnswer()}
+            style={styles.button}
+          />
+          <Button
+            onPress={this.state.numberToSelectCorrectAnswerButton == 1 ? this.handleCorrectResult : this.handleErrorResult}
+            title={this.state.numberToSelectCorrectAnswerButton == 1 ? this.state.correctAnswer.toString() : this.generateFakeAnswer()}
+            style={styles.button}
+          />
+          <Button
+            onPress={this.state.numberToSelectCorrectAnswerButton == 2 ? this.handleCorrectResult : this.handleErrorResult}
+            title={this.state.numberToSelectCorrectAnswerButton == 2 ? this.state.correctAnswer.toString() : this.generateFakeAnswer()}
+            style={styles.button}
+          />
+        </View>) : null}
+        {this.state.error != null ? (
+        <View>
+          <Text style={styles.errorText}>{this.state.error ? this.state.error : null}</Text>
+          <Button
+            onPress={this.restart}
+            title="Play Again"
+            style={styles.button}
+          />
+        </View>) : null}
       </View>
     );
   }
@@ -90,8 +117,22 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3F5765',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  operation: {
+    fontSize: 45,
+    color: '#EFEFEF',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#2B3A42',
+    color: '#EFEFEF',
+    padding: 10
+  },
+  errorText: {
+    fontSize: 80,
+    color: '#FF530D',
+  }
 });
